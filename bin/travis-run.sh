82eb58d7b360bd36120459ed63fe89c2ac5c81ff
David Read <david.read@hackneyworkshop.com>
2019-05-03 16:37:14 +0100

    Try putting flake8 in with the tests, to save startup time and simplify travis.yml

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,4 +1,8 @@
-#!/bin/sh -e
+#!/bin/sh -ex
+
+flake8 --version
+# stop the build if there are Python syntax errors or undefined names
+flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics --exclude ckan,ckanext-downloadall
 
 nosetests --ckan \
           --nologcapture \
@@ -8,3 +12,6 @@ nosetests --ckan \
           --cover-inclusive \
           --cover-erase \
           --cover-tests
+
+# strict linting
+flake8 . --count --max-complexity=10 --max-line-length=127 --statistics --exclude ckan,ckanext-downloadall
